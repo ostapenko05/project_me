c29360d366d8a643274621a3e3239dcc7d873b1d
Arkadiy <ostapenko_05@mail.ru>
2019-10-26 15:34:18 +0500

    изменил скилы

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ if (process.env.NODE_ENV === "development") {
     require("file-loader!./index.pug");
 }
 
-import "./scripts/skills";
+// import "./scripts/skills";
 import "./scripts/parallaxup";
 import "./scripts/parallaxbottom";
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,38 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+    template: "#skill",
+    props: ["skillName", "skillPercent"],
+    method: {
+        drawColoredCircle() {
+            const circle = this.$refs["color-circle"];
+            const dashArray = parseInt(
+                getComputedStyle(circle).getPropertyValue('stroke-dasharray')
+            );
+            const percent = (dashArray / 100) * (100 - this.skillPercent);
+
+            circle.style.strokeDashoffset = percent;
+        }
+    },
+    mounted() {
+        this.drawColoredCircle();
+    }
+};
+
+const skillRow = {
+    template: "#skills-row",
+    components: { skill },
+    props: ["skill"],
+};
+
+new Vue({
+    el: "#skills-component",
+    template: "#skills-list",
+    data: () => ({
+        skills: []
+    }),
+    comments: { skillRow },
+    created() {
+        this.skills = require('../data/skills.json');
+    }
+});
